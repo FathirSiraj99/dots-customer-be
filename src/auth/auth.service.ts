82cd687894d26ff809510926176928cdccbcd21d
Moaane <gpro0150@gmail.com>
2023-08-23 10:48:27 +0700

    .

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,16 @@ import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/co
 import { PrismaService } from 'src/prisma/prisma.service';
 import { user } from '@prisma/client'
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private prismaService: PrismaService) { }
+    constructor(
+        private prismaService: PrismaService,
+        private jwtService: JwtService
+    ) { }
 
-    async login(username: string, password: string): Promise<user> {
+    async login(username: string, password: string): Promise<{token: string}> {
 
         const user = await this.prismaService.user.findUnique({ where: { username } });
 
@@ -16,18 +20,26 @@ export class AuthService {
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
+
         if (!isPasswordValid) {
-            throw new UnauthorizedException('Invalid password');
+            throw new UnauthorizedException('Wrong Password');
         }
 
-        return user;
+        const payload = { sub: user.id, username: user.username }; 
+        const token = this.jwtService.sign(payload);
+
+        return { token };
     }
 
     async register(username: string, password: string): Promise<user> {
-        const isUserValid = await this.prismaService.user.findUnique({where: { username }});
+        const isUserValid = await this.prismaService.user.findUnique({ where: { username } });
 
         if (isUserValid) {
             throw new Error('User already exists');
+        }
+
+        if (password.length < 6) {
+            throw new Error('Password less than 6 characters')
         }
 
         const hashPassword = await bcrypt.hash(password, 8);
@@ -38,6 +50,8 @@ export class AuthService {
                 password: hashPassword,
             },
         });
+
+        
 
         return user;
     }
